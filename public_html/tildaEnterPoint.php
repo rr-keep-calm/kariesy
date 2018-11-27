9dd209fdcb4f721f9be7e7e803d66804d851a5d7
Валентин Ломов <help7@neox.ru>
2018-11-27 23:20:06 +0500

    Правка комментария

--- public_html/tildaEnterPoint.php
+++ public_html/tildaEnterPoint.php
@@ -7,7 +7,6 @@ $projects = [
 
 // TODO Отправляем запрос на принятие изменения соответствующему проекту
 if (isset($projects[$_GET['projectid']])) {
-  // Отдаём то что прислал плагин фиксации наличия старниц для экспорта
   $url = $projects[$_GET['projectid']] . '/export_check?_format=json&';
   $url .= http_build_query($_GET);
   $ch = curl_init();
@@ -15,6 +14,8 @@ if (isset($projects[$_GET['projectid']])) {
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   $output = json_decode(curl_exec($ch));
   curl_close($ch);
+
+  // Отдаём то что прислал плагин фиксации наличия страниц для экспорта
   if (is_array($output) && isset($output[0])) {
     echo $output[0];
   } else {
