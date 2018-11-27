2f6ce7d01347a5f5fdd19ed2f92bc2ef21e68333
Валентин Ломов <help7@neox.ru>
2018-11-27 23:07:49 +0500

    Первая версия точки входа для веб-хука от Тильды

--- public_html/tildaEnterPoint.php
+++ public_html/tildaEnterPoint.php
@@ -2,11 +2,22 @@
 // Соотносим идентификаторы проектов с существующими сайтами.
 // На одну точку входа должно быть не больше пяти сайтов.
 $projects = [
-  '931691' => 'http://kariesynet.tmweb.ru'
+  '931691' => 'http://dkariesy',
 ];
 
 // TODO Отправляем запрос на принятие изменения соответствующему проекту
 if (isset($projects[$_GET['projectid']])) {
-    // Сообщаем Тильде что запрос на обновление приняли
-    echo 'ok';
+  // Отдаём то что прислал плагин фиксации наличия старниц для экспорта
+  $url = $projects[$_GET['projectid']] . '/export_check?_format=json&';
+  $url .= http_build_query($_GET);
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  $output = json_decode(curl_exec($ch));
+  curl_close($ch);
+  if (is_array($output) && isset($output[0])) {
+    echo $output[0];
+  } else {
+    echo $output;
+  }
 }
