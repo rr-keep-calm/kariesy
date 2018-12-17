b55fdba00db9c7a1e99ebb126aa028452e1aa630
Валентин Ломов <help7@neox.ru>
2018-12-17 22:26:30 +0500

    Статичная версия страницы прайса через модуль и кастомный шаблон

--- public_html/modules/custom/price_page/src/Controller/PricePageController.php
+++ public_html/modules/custom/price_page/src/Controller/PricePageController.php
@@ -3,19 +3,19 @@
  * @file
  * Contains \Drupal\price_page\Controller\PricePageController.
  */
-namespace \Drupal\price_page\Controller;
+namespace Drupal\price_page\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
 class PricePageController extends ControllerBase {
 
   public function pricePage() {
-    $output = array();
 
-    $output['#title'] = 'Цены';
+    return [
+      '#theme' => 'price_page',
+      '#title' => 'Цены',
+      '#test_var' => $this->t('Test Value'),
+    ];
 
-    $output['#markup'] = 'Текст цен';
-
-    return $output;
   }
 }
