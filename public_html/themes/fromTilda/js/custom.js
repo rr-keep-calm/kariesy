939da5e046f0616455ecf945bf62b766df6e0e85
Валентин Ломов <help7@neox.ru>
2019-02-07 11:36:46 +0500

    ADD: показ и скрытие части текста на главной после первого абзаца

--- public_html/themes/fromTilda/js/custom.js
+++ public_html/themes/fromTilda/js/custom.js
@@ -7,4 +7,31 @@ $(document).ready(function() {
             $('.price_content' + valueSelected).show();
         })
     }
+
+    // Если мы на главной странице и есть текст больше одного абзаца
+    if ($('.home-text.page .wrapper').length && $('.home-text.page .wrapper').children().length <= 1) {
+        $('.home-text.page .wrapper .pagination-block_more').hide();
+    }
+    $('.home-text.page .wrapper').on('click', '.pagination-block_more.open', function(){
+        $(this).removeClass('open');
+        $(this).addClass('close');
+        $(this).text('Свернуть');
+        $.each($('.home-text.page .wrapper').children(), function(){
+            if (!$(this).hasClass('pagination-block_more')) {
+                $(this).show('slow');
+            }
+        });
+    });
+    $('.home-text.page .wrapper').on('click', '.pagination-block_more.close', function(){
+        $(this).removeClass('close');
+        $(this).addClass('open');
+        $(this).text('Развернуть');
+        var i = 0;
+        $.each($('.home-text.page .wrapper').children(), function(){
+            if (!$(this).hasClass('pagination-block_more') && i != 0) {
+                $(this).hide('slow');
+            }
+            i++;
+        });
+    });
 });
