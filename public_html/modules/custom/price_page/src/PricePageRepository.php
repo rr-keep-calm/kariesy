0ea9466d5532e2d8349f79a7e98ac42ca953ca3c
Валентин Ломов <nebudetvlom@gmail.com>
2019-07-04 12:08:01 +0500

    FIX: вывод цен на нескольких вкладках при соотнесении с несколькими категориями

--- public_html/modules/custom/price_page/src/PricePageRepository.php
+++ public_html/modules/custom/price_page/src/PricePageRepository.php
@@ -45,44 +45,57 @@ class PricePageRepository {
 
 
     foreach ($nodes as $node) {
-      $service_type_tid = $node->field_service_type->target_id;
-      $service_type2_tid = $node->field_service_type2->target_id;
-      if (!$service_type_tid || !$service_type2_tid) {
+      $service_type_tid_list = [];
+      $service_type_tids = $node->get('field_service_type');
+      foreach ($service_type_tids as $service_type_tid) {
+        $service_type_tid_list[] = $service_type_tid->target_id;
+      }
+
+      $service_type2_tid_list = [];
+      $service_type2_tids = $node->get('field_service_type2');
+      foreach ($service_type2_tids as $service_type2_tid) {
+        $service_type2_tid_list[] = $service_type2_tid->target_id;
+      }
+      if (!$service_type_tid_list || !$service_type2_tid_list) {
         continue;
       }
 
-      $service_type_term = Term::load($service_type_tid);
-      $service_type_term_name = $service_type_term->getName();
-      $service_type_term_weight = $service_type_term->getWeight();
+      foreach ($service_type_tid_list as $service_type_tid) {
+        foreach ($service_type2_tid_list as $service_type2_tid) {
+          $service_type_term = Term::load($service_type_tid);
+          $service_type_term_name = $service_type_term->getName();
+          $service_type_term_weight = $service_type_term->getWeight();
 
-      $service_type2_term = Term::load($service_type2_tid);
-      $service_type2_term_name = $service_type2_term->getName();
-      $service_type2_term_weight = $service_type2_term->getWeight();
+          $service_type2_term = Term::load($service_type2_tid);
+          $service_type2_term_name = $service_type2_term->getName();
+          $service_type2_term_weight = $service_type2_term->getWeight();
 
-      $translitiration = new PhpTransliteration();
-      $result[$service_type_tid]['name'] = $service_type_term_name;
-      $result[$service_type_tid]['weight'] = $service_type_term_weight;
-      $anchor = str_replace(' ', '_', $translitiration->transliterate($service_type_term_name, 'en', '_'));
-      $result[$service_type_tid]['anchor'] = $anchor;
-      $result[$service_type_tid]['types'][$service_type2_tid]['name'] = $service_type2_term_name;
-      $result[$service_type_tid]['types'][$service_type2_tid]['weight'] = $service_type2_term_weight;
+          $translitiration = new PhpTransliteration();
+          $result[$service_type_tid]['name'] = $service_type_term_name;
+          $result[$service_type_tid]['weight'] = $service_type_term_weight;
+          $anchor = str_replace(' ', '_', $translitiration->transliterate($service_type_term_name, 'en', '_'));
+          $result[$service_type_tid]['anchor'] = $anchor;
+          $result[$service_type_tid]['types'][$service_type2_tid]['name'] = $service_type2_term_name;
+          $result[$service_type_tid]['types'][$service_type2_tid]['weight'] = $service_type2_term_weight;
 
-      $node_variables = [
-        'name' => $node->getTitle(),
-        'description' => $node->body->value,
-        'price' => $node->field_cena->value,
-        'price_old' => $node->field_old_price->value,
-      ];
+          $node_variables = [
+            'name' => $node->getTitle(),
+            'description' => $node->body->value,
+            'price' => $node->field_cena->value,
+            'price_old' => $node->field_old_price->value,
+          ];
 
-      $price_to = $node->field_price_to->value;
-      if ($price_to) {
-        $node_variables['price_to'] = $price_to;
+          $price_to = $node->field_price_to->value;
+          if ($price_to) {
+            $node_variables['price_to'] = $price_to;
+          }
+
+          if ((bool) $node->field_price_from->value || $price_to) {
+            $node_variables['price_from'] = TRUE;
+          }
+          $result[$service_type_tid]['types'][$service_type2_tid]['prices'][] = $node_variables;
+        }
       }
-
-      if ((bool)$node->field_price_from->value || $price_to) {
-        $node_variables['price_from'] = true;
-      }
-      $result[$service_type_tid]['types'][$service_type2_tid]['prices'][] = $node_variables;
     }
 
     // Сортируем табы согласно весу в словаре
